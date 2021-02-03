2f6e7c6fe1fa3d7c57c8ffbd5877cf75ecae1e32
Egor Ilchenko <zzodoo@gmail.com>
2021-02-03 23:44:39 +0700

    Draft app state and render

--- client/src/zodo/jeopardy/client/AppState.scala
+++ client/src/zodo/jeopardy/client/AppState.scala
@@ -5,8 +5,28 @@ import zodo.jeopardy.model.PackModel
 sealed trait AppState
 
 object AppState {
-  case object BeforeStart extends AppState
-  case class ShowInfo(hash: String, pack: PackModel.Pack) extends AppState
-  case class QuestionDetails(hash: String, prev: AppState, question: PackModel.Question) extends AppState
-  case class Error(message: String) extends AppState
+  case object Anonymous extends AppState
+
+  case class InGame(gameInfo: GameInfo, gameState: GameState) extends AppState
+
+  sealed trait GameState
+  case class InRound(round: PackModel.Round, takenQuestions: Set[String]) extends GameState
+  case class InQuestion(question: PackModel.Question) extends GameState
+  case class InAnswer(answer: PackModel.Answers) extends GameState
+
+  case class GameInfo(
+    id: String,
+    packHash: String,
+    players: Seq[PlayerInfo]
+  ) {
+    def me: PlayerInfo = players.filter(_.me).head
+  }
+
+  case class PlayerInfo(
+    id: String,
+    name: String,
+    score: String,
+    givingAnswer: Boolean,
+    me: Boolean
+  )
 }
--- client/src/zodo/jeopardy/client/KorolevService.scala
+++ client/src/zodo/jeopardy/client/KorolevService.scala
@@ -1,10 +1,12 @@
 package zodo.jeopardy.client
 
+import zio.console._
 import korolev.Context
 import korolev.effect.Effect
 import korolev.server.{KorolevServiceConfig, StateLoader}
 import korolev.state.javaSerialization._
 import zodo.jeopardy.client.AppEvent.FileProcessed
+import zodo.jeopardy.client.AppState.{GameInfo, InAnswer, InQuestion, InRound, PlayerInfo}
 import zodo.jeopardy.model.PackModel
 import zodo.jeopardy.model.PackModel.Fragment.Image
 
@@ -21,7 +23,7 @@ class KorolevService(implicit eff: Effect[EnvTask], ec: ExecutionContext) {
   private val uploader = new Uploader
 
   val config = KorolevServiceConfig[EnvTask, AppState, Any](
-    stateLoader = StateLoader.default(AppState.BeforeStart),
+    stateLoader = StateLoader.default(AppState.Anonymous),
     document = state =>
       optimize {
         Html(
@@ -41,85 +43,29 @@ class KorolevService(implicit eff: Effect[EnvTask], ec: ExecutionContext) {
             TagDef("main")(
               style := "width: 1000px; margin: 0 auto",
               state match {
-                case AppState.BeforeStart =>
+                case AppState.Anonymous =>
                   uploader(()) { (access, event) =>
                     event match {
                       case FileProcessed(hash, pack) =>
-                        access.transition(_ => AppState.ShowInfo(hash, pack))
+                        putStrLn(s"File with $hash uploaded succesfully")
                     }
                   }
-                case AppState.ShowInfo(hash, pack) =>
+                case AppState.InGame(gi @ GameInfo(gameId, hash, players), gameState) =>
                   div(
-                    header(
-                      button(
-                        "Upload new",
-                        event("click")(_.transition(_ => AppState.BeforeStart))
-                      )
+                    h2(s"In game $gameId with pack $hash"),
+                    players.map(renderPlayer),
+                    gameState match {
+                      case InRound(round, takenQuestions) => renderInRound(round, takenQuestions)
+                      case InQuestion(question)           => renderInQuestion(hash, question)
+                      case InAnswer(answer)               => renderInAnswer(answer)
+                    },
+                    input(
+                      `type` := "text",
+                      if (!gi.me.givingAnswer) disabled else void
                     ),
-                    pack.rounds.map(round =>
-                      div(
-                        h2(round.name),
-                        when(round.typ == PackModel.RoundType.Final)(span("Final!")),
-                        tbody(
-                          round.themes.map(theme =>
-                            tr(
-                              style := "cursor: pointer",
-                              td(theme.name),
-                              theme.questions.map(question =>
-                                td(
-                                  question.price.toString(),
-                                  event("click")(_.transition(s => AppState.QuestionDetails(hash, s, question)))
-                                )
-                              )
-                            )
-                          )
-                        )
-                      )
-                    )
-                  )
-
-                case AppState.QuestionDetails(hash, prev, question) =>
-                  div(
-                    header(
-                      button(
-                        "Back",
-                        event("click")(access =>
-                          access.maybeTransition { case AppState.QuestionDetails(_, prev, _) => prev }
-                        )
-                      )
-                    ),
-                    h2("Question"),
-                    ul(
-                      question.fragments.map(fragment =>
-                        li(
-                          fragment match {
-                            case Image(url) =>
-                              img(
-                                width @= "600px",
-                                src := s"http://localhost:8080/media/$hash/Images/${url.drop(1)}"
-                              )
-                            case _ => fragment.toString
-                          }
-                        )
-                      )
-                    ),
-                    h2("Answer"),
-                    ul(question.answers.correct.map(answer => li(answer)))
-                  )
-                case AppState.Error(message) =>
-                  div(
-                    header(
-                      button(
-                        "Try again",
-                        event("click")(_.transition(_ => AppState.BeforeStart))
-                      )
-                    ),
-                    p(
-                      span(
-                        `class` := "label error",
-                        "Not OK"
-                      ),
-                      message
+                    button(
+                      if (!gi.me.givingAnswer) disabled else void,
+                      "Send"
                     )
                   )
               }
@@ -128,5 +74,69 @@ class KorolevService(implicit eff: Effect[EnvTask], ec: ExecutionContext) {
         )
       }
   )
+
+  private def renderPlayer(info: PlayerInfo) = {
+    val PlayerInfo(id, name, score, givingAnswer, me) = info
+
+    div(
+      ul(
+        h3(s"Player${if (me) "(its me!)" else ""}"),
+        li(s"Id - $id"),
+        li(s"Name - $name"),
+        li(s"Score - $score"),
+        li(s"Giving answer - $givingAnswer")
+      )
+    )
+  }
+
+  private def renderInRound(round: PackModel.Round, takenQuestions: Set[String]) = {
+    div(
+      h2(round.name),
+      when(round.typ == PackModel.RoundType.Final)(span("Final!")),
+      tbody(
+        round.themes.map(theme =>
+          tr(
+            style := "cursor: pointer",
+            td(theme.name),
+            theme.questions.map(question =>
+              td(
+                if (takenQuestions.contains(question.id))
+                  color @= "gray"
+                else void,
+                question.price.toString()
+              )
+            )
+          )
+        )
+      )
+    )
+  }
+
+  private def renderInQuestion(hash: String, question: PackModel.Question) = {
+    div(
+      h2("Question"),
+      ul(
+        question.fragments.map(fragment =>
+          li(
+            fragment match {
+              case Image(url) =>
+                img(
+                  width @= "600px",
+                  src := s"http://localhost:8080/media/$hash/Images/${url.drop(1)}"
+                )
+              case _ => fragment.toString
+            }
+          )
+        )
+      )
+    )
+  }
+
+  private def renderInAnswer(answer: PackModel.Answers) = {
+    div(
+      h2("Answer"),
+      ul(answer.correct.map(a => li(a)))
+    )
+  }
 
 }
